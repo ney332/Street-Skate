9e629d12a9f62a626bd90cd706df1ca72cf015e1
lorran <lorransantos@gmail.com>
2026-03-13 14:08:21 -0300

    Initial Commit

---
+++ SkateAppp/SkateApppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkateApppApp.swift
+//  SkateAppp
+//
+//  Created by Lorran on 13/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SkateApppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
